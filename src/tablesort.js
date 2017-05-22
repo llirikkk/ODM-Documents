d4c481aa765265167aa18bb5a3f053c54201ffe2
Kirill Yanchuk <arsenal_rulez@mail.ru>
2017-05-22 15:31:00 +0300

    Added reverse table sort

--- src/tablesort.js
+++ src/tablesort.js
@@ -7,26 +7,30 @@ const rows = [].slice.call(tBody.rows);
 // Table headings
 const tr = incDocTable.tHead.rows[0];
 
-// First heading
+// Current heading
 let target = tr.cells[0];
+let reverse = -1;
 
 tr.addEventListener("click",sortTable, false);
 
 function sortTable(event){
     if(target != event.target) {
-        target.classList.remove("th-sorted", "th-sorted_down");
+        target.classList.remove("th-sorted", "th-sorted_up");
         target = event.target;
-        target.classList.add("th-sorted", "th-sorted_down");
+        target.classList.add("th-sorted");
+        reverse = 1;
+    } else {
+        target.classList.toggle("th-sorted_up");
     }
     const index = target.cellIndex;
 
     // Stable sort of table rows.
     rows.sort(function(a, b){
        if(a.cells[index].textContent > b.cells[index].textContent){
-           return 1;
+           return reverse;
        }
         if(a.cells[index].textContent < b.cells[index].textContent){
-            return -1;
+            return -reverse;
         }
         // To provide stable sort: if elements are equal we keep them in the original order
         return a.rowIndex - b.rowIndex;
@@ -35,4 +39,5 @@ function sortTable(event){
         tBody.appendChild(rows[i]);
     }
     incDocTable.appendChild(tBody);
+    reverse = -reverse;
 }
