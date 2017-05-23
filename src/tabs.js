3c8ebd00e37a46709fd48e53d9901aa7739dd1e0
Kirill Yanchuk <arsenal_rulez@mail.ru>
2017-05-23 14:12:25 +0300

    Fixed bug with displaying current tab, when parameter "tab" in query is incorrect

--- src/tabs.js
+++ src/tabs.js
@@ -5,6 +5,6 @@ uri.replace(
     function($0, $1, $2, $3) { queryString[$1] = $3; }
     );
 let currentTabId = queryString["tab"] || "under-my-approval";
-let currentTab = document.getElementById(currentTabId);
+let currentTab = document.getElementById(currentTabId) || document.getElementById("under-my-approval");
 currentTab.classList.remove("tab-content_hide");
 console.log(currentTabId);
