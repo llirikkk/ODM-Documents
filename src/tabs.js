1f65981d860a4f955e5b4cbccf4787fd9ec93ed1
Kirill Yanchuk <arsenal_rulez@mail.ru>
2017-05-23 14:46:11 +0300

    Added event handler for tab-anchors. Added history.replaceState

--- src/tabs.js
+++ src/tabs.js
@@ -7,4 +7,13 @@ uri.replace(
 let currentTabId = queryString["tab"] || "under-my-approval";
 let currentTab = document.getElementById(currentTabId) || document.getElementById("under-my-approval");
 currentTab.classList.remove("tab-content_hide");
-console.log(currentTabId);
+
+const tabAnchors = document.querySelectorAll(".tab-links a");
+for (let i = 0, len = tabAnchors.length; i < len; i++){
+    tabAnchors[i].addEventListener("click", tabHandler);
+}
+
+function tabHandler(event) {
+    event.preventDefault();
+    history.replaceState("", "", "?tab=" + event.target.hash.slice(1));
+}
