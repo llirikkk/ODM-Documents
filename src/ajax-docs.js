571518918a0fb36c4f2623af0d7c664d7d97e5b4
Kirill Yanchuk <arsenal_rulez@mail.ru>
2017-05-26 14:37:38 +0300

    Changed the structure of created-by-me-inc.json

--- src/ajax-docs.js
+++ src/ajax-docs.js
@@ -2,7 +2,7 @@ function loadTabContent(tabId) {
     const tab = document.getElementById(tabId);
 
     // List of tables to be loaded
-    const tabTables = [`${tabId}-inc`, `${tabId}-out`, `${tabId}-fax`];
+    const tabTables = [`${tabId}-inc`];
 
     // Array of loaded tables
     let tableCache = [],
@@ -35,86 +35,86 @@ function loadTabContent(tabId) {
 
         // Initializing table heading
         heading.textContent = tableData["heading"];
-
-        // Index of table Execution marks
-        let markIndex;
-
-        let thArray = tableData["tHead"];
-        let tBodyArray = tableData["tBody"];
-
-        // Forming tHead
-        for (let i = 0, len = thArray.length; i < len; i++) {
-            let th = document.createElement("th");
-            th.innerHTML = thArray[i];
-            tHeadTr.appendChild(th);
-
-            const el = thArray[i].toLowerCase();
-            if (el === "execution mark" || el === "status") {
-                markIndex = i;
-            }
-        }
-
-        // Forming tBody
-        for (let i = 0, len = tBodyArray.length; i < len; i++) {
-            let tr = document.createElement("tr");
-
-            for (let j = 0, len = tBodyArray[i].length; j < len; j++) {
-                let td = document.createElement("td");
-
-                // Applying styles to Execution Marks
-                if (j === markIndex) {
-                    const mark = tBodyArray[i][j];
-                    let span = document.createElement("span");
-
-                    switch(mark.toLowerCase()) {
-                        case "signed":
-                            span.classList.add("table-mark", "table-mark_signed");
-                            break;
-                        case "awaits":
-                            span.classList.add("table-mark", "table-mark_awaits");
-                            break;
-                        case "cancelled":
-                            span.classList.add("table-mark", "table-mark_cancelled");
-                            break;
-                        case "approved":
-                            span.classList.add("table-mark", "table-mark_approved");
-                            break;
-                        case "held":
-                            span.classList.add("table-mark", "table-mark_held");
-                            break;
-                        case "changed":
-                            span.classList.add("table-mark", "table-mark_changed");
-                            break;
-                        case "deleted":
-                            span.classList.add("table-mark", "table-mark_deleted");
-                    }
-                    span.innerHTML = mark;
-                    td.appendChild(span);
-                } else {
-                    td.innerHTML = tBodyArray[i][j];
-                }
-                tr.appendChild(td);
-            }
-            tBody.appendChild(tr);
-        }
-
-        // Adding table with heading to the tab
-        table.id = `${tableName}__table`;
-        tHead.appendChild(tHeadTr);
-        table.appendChild(tHead);
-        table.appendChild(tBody);
-
-        // Adding heading to the table
-        tab.appendChild(heading);
-        tab.appendChild(table);
-
-        tableCache.push({
-            "heading": heading,
-            "table": table
-        });
-
-        // Init sort
-        initSort(table.id);
+        //
+        // // Index of table Execution marks
+        // let markIndex;
+        //
+        // let thArray = tableData["tHead"];
+        // let tBodyArray = tableData["tBody"];
+        //
+        // // Forming tHead
+        // for (let i = 0, len = thArray.length; i < len; i++) {
+        //     let th = document.createElement("th");
+        //     th.innerHTML = thArray[i];
+        //     tHeadTr.appendChild(th);
+        //
+        //     const el = thArray[i].toLowerCase();
+        //     if (el === "execution mark" || el === "status") {
+        //         markIndex = i;
+        //     }
+        // }
+        //
+        // // Forming tBody
+        // for (let i = 0, len = tBodyArray.length; i < len; i++) {
+        //     let tr = document.createElement("tr");
+        //
+        //     for (let j = 0, len = tBodyArray[i].length; j < len; j++) {
+        //         let td = document.createElement("td");
+        //
+        //         // Applying styles to Execution Marks
+        //         if (j === markIndex) {
+        //             const mark = tBodyArray[i][j];
+        //             let span = document.createElement("span");
+        //
+        //             switch(mark.toLowerCase()) {
+        //                 case "signed":
+        //                     span.classList.add("table-mark", "table-mark_signed");
+        //                     break;
+        //                 case "awaits":
+        //                     span.classList.add("table-mark", "table-mark_awaits");
+        //                     break;
+        //                 case "cancelled":
+        //                     span.classList.add("table-mark", "table-mark_cancelled");
+        //                     break;
+        //                 case "approved":
+        //                     span.classList.add("table-mark", "table-mark_approved");
+        //                     break;
+        //                 case "held":
+        //                     span.classList.add("table-mark", "table-mark_held");
+        //                     break;
+        //                 case "changed":
+        //                     span.classList.add("table-mark", "table-mark_changed");
+        //                     break;
+        //                 case "deleted":
+        //                     span.classList.add("table-mark", "table-mark_deleted");
+        //             }
+        //             span.innerHTML = mark;
+        //             td.appendChild(span);
+        //         } else {
+        //             td.innerHTML = tBodyArray[i][j];
+        //         }
+        //         tr.appendChild(td);
+        //     }
+        //     tBody.appendChild(tr);
+        // }
+        //
+        // // Adding table with heading to the tab
+        // table.id = `${tableName}__table`;
+        // tHead.appendChild(tHeadTr);
+        // table.appendChild(tHead);
+        // table.appendChild(tBody);
+        //
+        // // Adding heading to the table
+        // tab.appendChild(heading);
+        // tab.appendChild(table);
+        //
+        // tableCache.push({
+        //     "heading": heading,
+        //     "table": table
+        // });
+        //
+        // // Init sort
+        // initSort(table.id);
     }
 
     console.log(tableCache);
